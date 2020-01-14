56ec9ff0f8da57647e71b11a7d3dc4d8a60d91da
Morgaine <morgainedv@gmail.com>
2020-01-14 18:57:23 +0000

    adds default price method to rollercoaster

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -28,4 +28,11 @@ public class RollercoasterTest {
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
     }
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(8.40, rollerCoaster.defaultPrice(), 0.01);
+    }
+
+
 }
