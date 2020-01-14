091fee9041d0ab778d719bd72fcc70e39df7883a
Morgaine <morgainedv@gmail.com>
2020-01-14 19:57:51 +0000

    adds default price and price for method to dodgems

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp()  {
         dodgems = new Dodgems("Bumper Cars", 5);
+
+        visitor1 = new Visitor(25, 2.5, 20.00);
+        visitor2 = new Visitor(11, 0.5, 10.00);
     }
 
 
@@ -28,5 +34,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(4.50, dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void hasKidsPrice(){
+        assertEquals(2.25, dodgems.priceFor(visitor2), 0.01);
+    }
+
+    @Test
+    public void doesNotUnderchargeOverTwelves(){
+        assertEquals(4.50, dodgems.priceFor(visitor1), 0.01);
     }
 }
